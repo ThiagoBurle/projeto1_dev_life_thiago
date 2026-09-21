29eab4b3c8e7d8ca1462428f9103047c49d72a52
thiago burle <thiagoburle@gmail.com>
2026-09-21 16:34:44 -0300

    comecando o desenhando tela

--- codigo/inicializacao.py
+++ codigo/inicializacao.py
@@ -86,5 +86,6 @@ def inicializa_estado():
         'max_vidas': 5,  # Quantidade máxima de vidas que o jogador pode ter - o valor da chave 'vidas' nunca pode ser maior que o valor da chave 'max_vidas'
         'objetos': objetos,
         'mapa': mapa,
-        'mensagem': '',  # Use esta mensagem para mostrar mensagens ao jogador, como "Você perdeu uma vida" ou "Você ganhou uma vida"
+        'mensagem': '', # Use esta mensagem para mostrar mensagens ao jogador, como "Você perdeu uma vida" ou "Você ganhou uma vida"
+        'posicoes_ocupadas': posicoes_ocupadas
     }
--- codigo/tela_jogo.py
+++ codigo/tela_jogo.py
@@ -3,17 +3,51 @@ from constantes import *  # Você pode usar as constantes definidas em constante
                           # diretamente no código
 import motor_grafico as motor  # Utilize as funções do arquivo motor_grafico.py para desenhar na tela
                                # Por exemplo: motor.preenche_fundo(janela, [0, 0, 0]) preenche o fundo de preto
-
+from inicializacao import gera_posicao_desocupada
 
 def desenha_tela(janela, estado, altura_tela, largura_tela):
     # Utilize o dicionário estado para saber onde o jogador e os outros objetos estão.
     # Por exemplo, para saber a posição do jogador, use estado['pos_jogador']
     # O mapa esta armazenado em estado['mapa'].
     motor.preenche_fundo(janela, PRETO)
+    obejetos = estado['objetos']
+    mapa = estado['mapa']
+    largura_mapa = len(mapa[0])
+    altura_mapa = len(mapa)
+    posiçoes = estado['posicoes_ocupadas']
+    dx = (largura_tela - largura_mapa)// 2
+    dy = (altura_tela - altura_mapa) // 2
+    for i in range(len(mapa)): 
+        for j in range(len(mapa[i])):
+            fundo = VERDE_CLARO
+            frente = VERDE_CLARO
+            # v =  i + j 
+            # if v%2 == 0:
+            #     fundo = VERDE_ESCURO
+            #     frente = VERDE_CLARO
+            motor.desenha_string(janela, j + dx,i + dy, mapa[i][j], frente, fundo)
+            motor.desenha_string(janela, 0 ,0, estado['vidas'] * (CORACAO + ' '), PRETO, VERMELHO )
+            motor.desenha_string(janela, 0,altura_tela - 1, estado['mensagem'], PRETO, BRANCO )
+            
+    for objeto in obejetos:
+        posicao = gera_posicao_desocupada(posiçoes,largura_mapa, altura_mapa)
+        if objeto['tipo'] == CORACAO:
+            motor.desenha_string(janela, posicao[0]+dx,posicao[1]+dy,objeto['tipo'], frente, VERMELHO )
+        else:
+            pass
+    for objeto in obejetos:
+            posicao = gera_posicao_desocupada(posiçoes,largura_mapa, altura_mapa)
+            if objeto['tipo'] == ESPINHO:
+                motor.desenha_string(janela, posicao[0]+dx,posicao[1]+dy,objeto['tipo'], frente, VERDE_ESCURO )
+            else:
+                pass
+
+    
+
     
     # O seu código deve desenhar a tela do jogo aqui a partir dos valores no dicionário "estado"
     # APAGUE ESTA LINHA E A LINHA ABAIXO E ESCREVA SEU CÓDIGO AQUI
-    motor.desenha_string(janela, 0, altura_tela // 2, 'APAGUE ESTA LINHA', PRETO, BRANCO)
+
 
     motor.mostra_janela(janela)
 
